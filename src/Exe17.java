6718986df9c03bc8c884daf16d1c04f814f246dd
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-03-10 16:32:01 -0300

    Atividades em sala de aula.

---
+++ src/Exe17.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+
+public class Exe17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho, em metros quadrados, da área a ser pintada: ");
+        Double metros = scanner.nextDouble();
+        System.out.println("");
+
+        Double quantidadeLitrosDeTinta = obterQuantidadeLitrosDeTinta(metros);
+        Double quantidadeLatasDeTinta18L = obterQuantidadeLatasDeTinta18L(quantidadeLitrosDeTinta);
+        Double precoTotalLata = obterPrecoTotalLata(quantidadeLatasDeTinta18L);
+        Double lataTintaMenor = obterLataTintaMenor(quantidadeLitrosDeTinta);
+        Double precoTotalLataMenor = obterPrecoTotalLataMenor(lataTintaMenor);
+        Double precoGalaoELata = obterPrecoGalaoELata(precoTotalLata, precoTotalLataMenor);
+        Double precoGalaoELataComFolga = obterPrecoGalaoELataComFolga(precoGalaoELata);
+
+        System.out.println("Latas de tinta de 18 litros a serem compradas: " + quantidadeLatasDeTinta18L);
+        System.out.println("Preço total em latas de 18 litros: " + precoTotalLata);
+        System.out.println("Latas de tinta de 3,6 litros a serem compradas: " + lataTintaMenor);
+        System.out.println("Preço total com latas de 3,6 litros: " + precoTotalLataMenor);
+        System.out.println("Preço misturado com 10% de folga: " + precoGalaoELataComFolga);
+
+       scanner.close();
+
+    }
+
+    private static double obterPrecoGalaoELataComFolga(Double precoGalaoELata) {
+        return Math.ceil(precoGalaoELata - (precoGalaoELata * 10) / 100);
+    }
+
+    private static double obterPrecoGalaoELata(Double precoTotalLata, Double precoTotalLataMenor) {
+        return Math.ceil(precoTotalLata + precoTotalLataMenor);
+    }
+
+    private static double obterPrecoTotalLataMenor(Double lataTintaMenor) {
+        return Math.ceil(lataTintaMenor *25);
+    }
+
+    private static double obterLataTintaMenor(Double quantidadeLitrosDeTinta) {
+        return Math.ceil(quantidadeLitrosDeTinta / 3.6);
+    }
+
+    private static double obterPrecoTotalLata(Double quantidadeLatasDeTinta18L) {
+        return Math.ceil(quantidadeLatasDeTinta18L * 80);
+    }
+
+    private static double obterQuantidadeLatasDeTinta18L(Double quantidadeLitrosDeTinta) {
+        return Math.ceil(quantidadeLitrosDeTinta / 18);
+    }
+
+    private static double obterQuantidadeLitrosDeTinta(Double metros) {
+        return Math.ceil(metros / 6);
+    }
+
+}
---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        String variavelString = ""; // o tipo String não possui um tipo primitivo equivalente.
+
+        byte variavelByte = 1;  // o tipo byte, assim como outros primitivos,
+                                // não pode manter um estado de ausência de valor.
+                                // Lembrando que ausência de valor é representado pela palavra null
+                                // que significa nulo.
+
+        short variavelshort = 1;
+
+        int variavelInteger = 1; // o tipo normal de int é Integer.
+
+        long variavelLong = 1L;
+
+        float variavelfloat = 1.0f;
+
+        double variaveldouble = 1.0;
+
+        char variavelCharacter = 'A'; // o tipo normal de char é Character.
+
+        boolean variavelBoolean = true;
+
+    }
+}
