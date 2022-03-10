acd447f5af2eb248859596770896d551e5f3305b
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-03-10 16:56:04 -0300

    Final

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Exercicio03 {
+    static final Integer PASSOU_DE_ANO = 70;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite a sua nota: ");
-        Double notaDoAluno = scanner.nextDouble();
+        double notaDoAluno = scanner.nextDouble();
 
-        Boolean passouDeAno = notaDoAluno >= 70;
+        boolean passouDeAno = notaDoAluno >= PASSOU_DE_ANO;
 
         if (passouDeAno) {
             System.out.println("Parabéns! Você passou de ano.");
