85859dd269f85ba64e5b2d46ab11c4bf52f1702b
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-03-10 16:44:51 -0300

    Atividades em sala de aula.

--- src/Exercicio05.java
+++ src/Exercicio05.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
-public class Exercicio03 {
+import java.util.Scanner;
+
+public class Exercicio05 {
+    static final Integer PASSOU_DE_ANO = 70;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite a sua nota: ");
         Double notaDoAluno = scanner.nextDouble();
 
-        Boolean passouDeAno = notaDoAluno >= 70;
+        Boolean passouDeAno = notaDoAluno >= PASSOU_DE_ANO;
 
         if (passouDeAno) {
             System.out.println("Parabéns! Você passou de ano.");
