6c158874068dca3528a92a9f7994022b3dc33cc0
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-03-31 17:17:54 -0300

    Final

--- src/com/company/EstruturaDecisaoExe14.java
+++ src/com/company/EstruturaDecisaoExe14.java
@@ -13,8 +13,38 @@ public class EstruturaDecisaoExe14 {
 
         double mediaDasNotasEmIngles = (primeiraNotaEmIngles + segundaNotaEmIngles) /2;
 
-        if (mediaDasNotasEmIngles == 9 && mediaDasNotasEmIngles == 10){
-
+        if (mediaDasNotasEmIngles >= 9.0 && mediaDasNotasEmIngles <= 10.0){
+            System.out.println("Sua primeira nota foi: " + primeiraNotaEmIngles);
+            System.out.println("Sua segunda nota foi: " + segundaNotaEmIngles);
+            System.out.println("Sua média foi: " + mediaDasNotasEmIngles);
+            System.out.println("Você tirou um 'A'!");
+            System.out.println("Aprovado!");
+        } else if (mediaDasNotasEmIngles >=7.5 && mediaDasNotasEmIngles < 9.0){
+            System.out.println("Sua primeira nota foi: " + primeiraNotaEmIngles);
+            System.out.println("Sua segunda nota foi: " + segundaNotaEmIngles);
+            System.out.println("Sua média foi: " + mediaDasNotasEmIngles);
+            System.out.println("Você tirou um 'B'!");
+            System.out.println("Aprovado!");
+        } else if (mediaDasNotasEmIngles >=6.5 && mediaDasNotasEmIngles < 7.5){
+            System.out.println("Sua primeira nota foi: " + primeiraNotaEmIngles);
+            System.out.println("Sua segunda nota foi: " + segundaNotaEmIngles);
+            System.out.println("Sua média foi: " + mediaDasNotasEmIngles);
+            System.out.println("Você tirou um 'C'!");
+            System.out.println("Aprovado!");
+        } else if (mediaDasNotasEmIngles >= 4.0 && mediaDasNotasEmIngles <= 6.0){
+            System.out.println("Sua primeira nota foi: " + primeiraNotaEmIngles);
+            System.out.println("Sua segunda nota foi: " + segundaNotaEmIngles);
+            System.out.println("Sua média foi: " + mediaDasNotasEmIngles);
+            System.out.println("Você tirou um 'D'!");
+            System.out.println("Reprovado!");
+        } else if (mediaDasNotasEmIngles >= 4.0 && mediaDasNotasEmIngles == 0){
+            System.out.println("Sua primeira nota foi: " + primeiraNotaEmIngles);
+            System.out.println("Sua segunda nota foi: " + segundaNotaEmIngles);
+            System.out.println("Sua média foi: " + mediaDasNotasEmIngles);
+            System.out.println("Você tirou um 'E'!");
+            System.out.println("Reprovado!");
         }
+
+        scanner.close();
     }
 }
---
+++ src/com/company/EstruturaDecisaoExe15.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class EstruturaDecisaoExe15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe um valor para o primeiro lado de um triângulo: ");
+        double primeiroLadoDoTriangulo = scanner.nextDouble();
+        System.out.print("Informe um valor para o segundo lado de um triângulo: ");
+        double segundoLadoDoTriangulo = scanner.nextDouble();
+        System.out.print("Informe um valor para o terceiro lado de um triângulo: ");
+        double terceiroLadoDoTriangulo = scanner.nextDouble();
+
+
+        if (eUmTriangulo(primeiroLadoDoTriangulo, segundoLadoDoTriangulo, terceiroLadoDoTriangulo)) {
+            System.out.println("\nÉ um triângulo.");
+
+            if (trianguloequilatero(primeiroLadoDoTriangulo, segundoLadoDoTriangulo, terceiroLadoDoTriangulo)) {
+                System.out.println("\nÉ um triângulo Equilátero.");
+
+            } else if (trianguloIsosceles(primeiroLadoDoTriangulo, segundoLadoDoTriangulo, terceiroLadoDoTriangulo)) {
+                System.out.println("\nÉ um triângulo Isósceles.");
+
+            } else if (trianguloEscaleno(primeiroLadoDoTriangulo, segundoLadoDoTriangulo, terceiroLadoDoTriangulo)) {
+                System.out.println("\nÉ um triângulo Escaleno.");
+            }
+
+        } else {
+            System.out.println("\nNão é um triângulo.");
+        }
+        scanner.close();
+}
+
+    private static boolean eUmTriangulo(double primeiroLadoDoTriangulo, double segundoLadoDoTriangulo, double terceiroLadoDoTriangulo) {
+        return primeiroLadoDoTriangulo + segundoLadoDoTriangulo > terceiroLadoDoTriangulo &&
+                primeiroLadoDoTriangulo + terceiroLadoDoTriangulo > segundoLadoDoTriangulo &&
+                segundoLadoDoTriangulo + terceiroLadoDoTriangulo > primeiroLadoDoTriangulo;
+    }
+
+    private static boolean trianguloequilatero(double primeiroLadoDoTriangulo, double segundoLadoDoTriangulo, double terceiroLadoDoTriangulo) {
+        return primeiroLadoDoTriangulo == segundoLadoDoTriangulo &&
+                primeiroLadoDoTriangulo == terceiroLadoDoTriangulo &&
+                segundoLadoDoTriangulo == terceiroLadoDoTriangulo;
+    }
+
+    private static boolean trianguloIsosceles(double primeiroLadoDoTriangulo, double segundoLadoDoTriangulo, double terceiroLadoDoTriangulo) {
+        return primeiroLadoDoTriangulo == segundoLadoDoTriangulo ||
+                primeiroLadoDoTriangulo == terceiroLadoDoTriangulo ||
+                terceiroLadoDoTriangulo == segundoLadoDoTriangulo;
+    }
+
+    private static boolean trianguloEscaleno(double primeiroLadoDoTriangulo, double segundoLadoDoTriangulo, double terceiroLadoDoTriangulo) {
+        return primeiroLadoDoTriangulo != segundoLadoDoTriangulo ||
+                primeiroLadoDoTriangulo != terceiroLadoDoTriangulo ||
+                terceiroLadoDoTriangulo != segundoLadoDoTriangulo;
+    }
+
+
+
+}
+
