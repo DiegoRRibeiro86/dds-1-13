967560c4c894a5efd2448f1ee41796735d34547a
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-04-07 16:50:03 -0300

    Final

---
+++ src/com/company/iteração/IteracaoWhile.java
@@ -0,0 +1,28 @@
+package com.company.iteração;
+
+public class IteracaoWhile {
+    public static void main(String[] args) {
+/*        for (int i = 0; i < 10; i++) {
+            System.out.println("Eu amo JAva!");
+        }
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Eu amo Java!");
+            i++;
+        }*/
+
+/*        int i = 0;
+        while (i < 10) {
+            if ( i == 5) {
+                System.out.println("Vai parar!");
+                break;
+            }
+            System.out.println("Eu amo Java!");
+            i++;
+        }*/
+
+            }
+        }
+
+
