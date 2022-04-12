ed353e5cadd022df5608e217fb0d9fdd0e41a240
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-04-12 16:00:22 -0300

    Final

--- src/com/company/vetores/Exercicio01.java
+++ src/com/company/vetores/Exercicio01.java
@@ -10,13 +10,18 @@ import java.util.Scanner;
 public class Exercicio01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tarefasDiarias = new String[5];
+        String[] tarefas = new String[5];
 
-        for (int i =0; i < tarefasDiarias.length ; i++) {
-            System.out.println("Digite na posição " + i + " sua tarefa diária: ");
-            String tarefa = scanner.next();
+        for (int i = 0; i < tarefas.length ; i++) {
+            System.out.println("Tarefa " + i + ": ");
+            tarefas[i] = scanner.nextLine();
         }
-        System.out.println(tarefasDiarias);
+
+        System.out.println("Suas tarefas são: ");
+        for (int i = 0; i < tarefas.length; i++) {
+            System.out.println(i + ": " + tarefas[i]);
+        }
+        scanner.close();
     }
 
 }
