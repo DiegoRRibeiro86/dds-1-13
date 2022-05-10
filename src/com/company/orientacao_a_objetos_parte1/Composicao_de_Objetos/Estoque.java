9af5ed21ebbad3ca97923b06a0d8cf47e00b62a5
DiegoRRibeiro86 <diego.executive@gmail.com>
2022-05-10 17:22:38 -0300

    Final

---
+++ src/com/company/orientacao_a_objetos_parte1/Composicao_de_Objetos/Estoque.java
@@ -0,0 +1,20 @@
+package com.company.orientacao_a_objetos_parte1.Composicao_de_Objetos;
+
+import java.util.Scanner;
+
+public class Estoque {
+    public static void main(String[] args) {
+
+        String arrayDeProdutos;
+        String produtos;
+
+        System.out.println("Quantos produtos deseja cadastrar: ");
+        double nprodutos =
+
+    for (int i = 0; i < ; i++) {
+        System.out.println("Digite o nome do produto: ");
+
+
+    }
+    }
+}
